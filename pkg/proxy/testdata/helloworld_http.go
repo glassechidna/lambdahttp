b75d16364fbe534d51230c5ffb31772512450be4
Aidan Steele <aidan.steele@glassechidna.com.au>
2020-01-24 15:31:01 +1100

    Rely on secret from parameter store

--- pkg/proxy/testdata/helloworld_http.go
+++ pkg/proxy/testdata/helloworld_http.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		bytes, err := httputil.DumpRequest(r, true)
 		fmt.Println(string(bytes), err)
-		w.Write([]byte(`pong`))
+		w.Write([]byte(os.Getenv("RESPONSE")))
 	})
 
 	panic(http.ListenAndServe(":8080", nil))
